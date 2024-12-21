8df386d6a3e17539e6531b93aa59e3fe064a0093
DuongWuangDat <duongquangdat172004@gmail.com>
2024-12-21 08:44:17 +0700

    fix extension

--- extension/background.js
+++ extension/background.js
@@ -1,39 +1,39 @@
-const url = "http://localhost:3000/api/v1/search";
+const url = "http://localhost:4000/api/v1/search";
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-    if (message.action == "fetchData") {
-        const body = {
-            keyword: message.word,
-            context: message.context,
-        };
-        try {
-            const response = await fetch(url, {
-                method: "POST",
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(body),
-            });
-            if (response.ok) {
-                const res = await response.json();
-                chrome.tabs.sendMessage(sender.tab.id, {
-                    success: true,
-                    data: res.data,
-                });
-            } else {
-                chrome.tabs.sendMessage(sender.tab.id, {
-                    success: false,
-                    message: "Something went wrong",
-                });
-            }
-        } catch (e) {
-            chrome.tabs.sendMessage(sender.tab.id, {
-                success: false,
-                message: "Something went wrong",
-            });
-        }
-    }
+  if (message.action == "fetchData") {
+    const body = {
+      keyword: message.word,
+      context: message.context,
+    };
 
-    return true;
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      if (response.ok) {
+        const res = await response.json();
+        chrome.tabs.sendMessage(sender.tab.id, {
+          success: true,
+          data: res.data,
+        });
+      } else {
+        chrome.tabs.sendMessage(sender.tab.id, {
+          success: false,
+          message: "Something went wrong",
+        });
+      }
+    } catch (e) {
+      chrome.tabs.sendMessage(sender.tab.id, {
+        success: false,
+        message: "Something went wrong",
+      });
+    }
+  }
+
+  return true;
 });
